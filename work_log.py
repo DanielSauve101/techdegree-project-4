c5e6c73d54edc9958af53086609785ef774916a2
Daniel Sauve <sauve.daniel.157@gmail.com>
2019-05-01 16:09:56 -0400

    added search by employee function and search menu

--- work_log.py
+++ work_log.py
@@ -20,7 +20,7 @@ class Entry(Model):
     task_title = CharField(max_length=100)
     employee_name = CharField(max_length=100)
     time_spent = IntegerField()
-    optional_notes = TextField()
+    optional_notes = TextField(null=False)
     timestamp = TimestampField(default=datetime.datetime.now)
 
     class Meta:
@@ -37,20 +37,36 @@ def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def menu_loop():
-    """Show the menu"""
+def main_menu_loop():
+    """Show the main menu"""
     choice = None
 
     while choice != 'q':
         clear()
         print("Enter 'q' to quit.")
-        for key, value in menu.items():
+        for key, value in main_menu.items():
             print('{}) {}'.format(key, value.__doc__))
         choice = input('Choice: ').lower().strip()
 
-        if choice in menu:
+        if choice in main_menu:
             clear()
-            menu[choice]()
+            main_menu[choice]()
+
+
+def search_menu_loop():
+    """Search entries"""
+    choice = None
+
+    while choice != 'q':
+        clear()
+        print("Enter 'q' to return to previous menu.")
+        for key, value in search_menu.items():
+            print('{}) {}'.format(key, value.__doc__))
+        choice = input('Choice: ').lower().strip()
+
+        if choice in search_menu:
+            clear()
+            search_menu[choice]()
 
 
 def add_entry():
@@ -72,16 +88,14 @@ def add_entry():
             print("Saved successfully!")
 
 
-def search_entries():
-    """Search entries"""
-    pass
-
-
-def view_entries():
+def view_entries(search_query=None):
     """View entries"""
     entries = Entry.select().order_by(Entry.timestamp.desc())
 
-    for entry in Entry.select():
+    if search_query:
+        entries = search_query
+
+    for entry in entries:
         timestamp = entry.timestamp.strftime('%B %d %Y, %I:%M%p')
         clear()
         print("Date entered: {}".format(timestamp))
@@ -93,9 +107,9 @@ def view_entries():
         print('\n'+'='*len(timestamp))
         print('n) next entry')
         print('d) delete entry')
-        print('q) return to main menu')
+        print('q) return to previous menu')
 
-        next_action = input('Action: [Ndq] ').lower().strip()
+        next_action = input('Choice: [Ndq] ').lower().strip()
         if next_action == 'q':
             break
         elif next_action == 'd':
@@ -108,13 +122,48 @@ def delete_entry(entry):
         entry.delete_instance()
 
 
-menu = OrderedDict([
+def search_by_employee():
+    """Search by employee name"""
+    employee = input('Enter employee name: ')
+    entries = Entry.select().where(Entry.employee_name.contains(employee))
+    view_entries(entries)
+
+
+def search_by_date():
+    """Search by specific date"""
+    pass
+
+
+def search_by_range_of_dates():
+    """Search by range of dates"""
+    pass
+
+
+def search_by_time_spent():
+    """Search by time spent"""
+    pass
+
+
+def search_by_word():
+    """Search by word"""
+    pass
+
+
+main_menu = OrderedDict([
     ('a', add_entry),
-    ('b', search_entries),
+    ('b', search_menu_loop),
     ('v', view_entries),
+])
+
+search_menu = OrderedDict([
+    ('a', search_by_employee),
+    ('b', search_by_date),
+    ('c', search_by_range_of_dates),
+    ('d', search_by_time_spent),
+    ('e', search_by_word),
 ])
 
 
 if __name__ == '__main__':
     initialize()
-    menu_loop()
+    main_menu_loop()
