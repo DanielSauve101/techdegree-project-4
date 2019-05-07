16b943e9df23dc7569b049b2b52091e206993e79
Daniel Sauve <sauve.daniel.157@gmail.com>
2019-05-07 11:13:45 -0400

    completed tests for add_entry function

--- tests.py
+++ tests.py
@@ -16,11 +16,26 @@ class WorklogTests(unittest.TestCase):
         test_db.connect(reuse_if_open=True)
         test_db.create_tables(MODELS, safe=True)
 
-    def test_add_entry(self):
+    def test_valid_add_entry(self):
         inputs_to_pass = ['Job1', 'Daniel', 60, 'This is a test', 'y']
         with patch('builtins.input', side_effect=inputs_to_pass):
-            result = work_log.add_entry()
-            self.assertEqual(result, inputs_to_pass)
+            with patch('builtins.print', side_effect=print) as mock:
+                result = work_log.add_entry()
+                mock.assert_called_once_with('\nEntry saved successfully!')
+
+    def test_not_valid_time_add_entry(self):
+        inputs_to_pass = ['Job1', 'Daniel', 'not integer', 60, 'This is a test', 'n']
+        with patch('builtins.input', side_effect=inputs_to_pass):
+            with patch('builtins.print', side_effect=print) as mock:
+                result = work_log.add_entry()
+                mock.assert_called_once_with('\nValue must be a number. Please try again.')
+
+    def test_not_valid_save_add_entry(self):
+        inputs_to_pass = ['Job1', 'Daniel', 60, 'This is a test', 'asdf', 'n']
+        with patch('builtins.input', side_effect=inputs_to_pass):
+            with patch('builtins.print', side_effect=print) as mock:
+                result = work_log.add_entry()
+                mock.assert_called_once_with('\nYou must enter [Yn]')
 
 
 if __name__ == '__main__':
