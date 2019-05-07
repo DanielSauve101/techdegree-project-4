411284ebc7c474e5719b16e1dbd7bdc6b091e6b5
Daniel Sauve <sauve.daniel.157@gmail.com>
2019-05-07 15:19:56 -0400

    test searchnametests 1 part complete.

--- tests.py
+++ tests.py
@@ -15,7 +15,14 @@ class WorklogTests(unittest.TestCase):
         test_db.bind(MODELS)
         test_db.connect(reuse_if_open=True)
         test_db.create_tables(MODELS, safe=True)
+        work_log.Entry.create(task_title='Job1',
+                              employee_name='Daniel',
+                              time_spent=60,
+                              optional_notes='This is a test'
+                              )
 
+
+class AddEntryTests(WorklogTests, unittest.TestCase):
     def test_valid_add_entry(self):
         inputs_to_pass = ['Job1', 'Daniel', 60, 'This is a test', 'y']
         with patch('builtins.input', side_effect=inputs_to_pass):
@@ -36,6 +43,23 @@ class WorklogTests(unittest.TestCase):
             with patch('builtins.print', side_effect=print) as mock:
                 result = work_log.add_entry()
                 mock.assert_called_once_with('\nYou must enter [Yn]')
+
+
+class SearchNameTests(WorklogTests, unittest.TestCase):
+    def test_not_valid_search_by_employee_name(self):
+        inputs_to_pass = ['Bob']
+        with patch('builtins.input', side_effect=inputs_to_pass):
+            with patch('builtins.print', side_effect=print) as mock:
+                result = work_log.search_by_employee()
+                mock.assert_called_once_with(
+                    '\nNo employee matches found for {}'.format(inputs_to_pass[0]))
+
+    def test_valid_search_by_employee_name(self):
+        inputs_to_pass = ['Daniel', 'q', 'q']
+        with patch('builtins.input', side_effect=inputs_to_pass):
+            result = work_log.search_by_employee()
+            self.assertIn(inputs_to_pass[0], work_log.Entry.select().where(
+                work_log.Entry.employee_name.contains(inputs_to_pass[0])))
 
 
 if __name__ == '__main__':
